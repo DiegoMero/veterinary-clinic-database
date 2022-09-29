93db69fa21a9c85b862bdda8219af349ea349b7b
Diego Mero <diegomero_1999@hotmail.com>
2022-09-29 02:37:16 -0500

    Add queries.sql new data

--- queries.sql
+++ queries.sql
@@ -60,8 +60,52 @@ SELECT species, MIN(weight_kg), MAX(weight_kg)
 FROM animals
 GROUP BY species;
 
-
 SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth BETWEEN 'Jan 1, 1990' AND 'Dec 31, 2000'
 GROUP BY species;
+
+
+SELECT owner_id, name, full_name
+FROM animals
+JOIN owners
+ON animals.owner_id = owners.id
+WHERE full_name = 'Melody Pond';
+
+SELECT species_id, animals.name, species.name
+FROM animals
+JOIN species
+ON animals.species_id = species.id
+WHERE species.name = 'Pokemon';
+
+SELECT owners.id, owners.full_name, animals.name
+FROM owners
+LEFT JOIN animals
+ON owners.id = animals.owner_id;
+
+SELECT species.name, COUNT(animals.id)
+FROM species
+JOIN animals
+ON species.id = animals.species_id
+GROUP BY species.name;
+
+SELECT owners.full_name, animals.name, species.name
+FROM owners
+JOIN animals
+ON owners.id = animals.owner_id
+JOIN species
+ON species.id = animals.species_id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
+
+SELECT animals.name, owners.full_name, animals.escape_attempts
+FROM animals
+JOIN owners
+ON owners.id = animals.owner_id
+WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts < 1;
+
+SELECT owners.full_name, COUNT(animals.id)
+FROM owners
+JOIN animals
+ON owners.id = animals.owner_id
+GROUP BY owners.full_name
+ORDER BY COUNT(animals.id) DESC;
